38fa16af06cfc2948379b40db798386b0ab751cf
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-04-13 00:14:08 +0200

    Commit DEBUG_MACHINE

---
+++ Assets/Scripts/Diagnostics/ScreenDebugLogger.cs
@@ -0,0 +1,45 @@
+﻿using PixelCrushers.DialogueSystem;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenDebugLogger : MonoBehaviour
+{
+    private List<Variable> variables;
+    private string myLog = string.Empty;
+    private bool isPanelActive;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isPanelActive = !isPanelActive;
+            if (isPanelActive)
+            {
+                myLog = string.Empty;
+                UpdatePanel();
+            }
+        }
+    }
+
+    private void UpdatePanel()
+    {
+        variables = DialogueManager.Instance.DatabaseManager.MasterDatabase.variables;
+        variables.ForEach(hV =>
+        {
+            myLog += "[" + hV.Type + "]";
+            hV.fields.ForEach(hF =>
+            {
+                myLog += hF.value + " ";
+            });
+            myLog += "\n";
+        });
+    }
+
+    public void OnGUI()
+    {
+        if (isPanelActive)
+        {
+            GUI.TextArea(new Rect(10, 10, Screen.width - 10, Screen.height - 10), myLog);
+        }
+    }
+}
