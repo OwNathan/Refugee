67d8072ca23c0994b9960e53c5b3f2ebbb209d23
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-04-13 20:42:40 +0200

    Commit VariableDebugger ;D

--- Assets/Scripts/Diagnostics/DebuggerDatabaseVarList.cs
+++ Assets/Scripts/Diagnostics/DebuggerDatabaseVarList.cs
@@ -2,34 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScreenDebugLogger : MonoBehaviour
+public class DebuggerDatabaseVarList : MonoBehaviour
 {
+    private List<string> variables;
     private string myLog = string.Empty;
     private bool isPanelActive;
+
+    public void Awake()
+    {
+        variables = new List<string>();
+    }
+
+    public void Start()
+    {
+        DialogueManager.Instance.initialDatabase.variables.ForEach(hVar => variables.Add(hVar.Name));
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             isPanelActive = !isPanelActive;
+
             if (isPanelActive)
-            {
-                myLog = string.Empty;
                 UpdatePanel();
-            }
         }
     }
 
     private void UpdatePanel()
     {
-        Debug.Log(DialogueLua.GetVariable("Firefight.Soldiers").AsTable);
+        myLog = string.Empty;
+        variables.ForEach(hVar => myLog += hVar + ": " + DialogueLua.GetVariable(hVar).AsInt + "\n");
     }
 
     public void OnGUI()
     {
         if (isPanelActive)
-        {
             GUI.TextArea(new Rect(10, 10, Screen.width - 10, Screen.height - 10), myLog);
-        }
     }
 }
