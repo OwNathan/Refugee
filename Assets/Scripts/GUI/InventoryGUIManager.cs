cf0bed5c2a8a20e93f9f09c2d0bdb289a43b5a26
EnricoCempanari <enrico.cempanari@gmail.com>
2016-04-17 18:54:58 +0200

    First inventory prototype
    Inventory is working but with some downsides:
    - 30 items instead of 32 (AC don't support more than30)
    - No drag and drop UI (need more understanding of how AC works

---
+++ Assets/Scripts/GUI/InventoryGUIManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryGUIManager : MonoBehaviour
+{
+
+    public Animator InventoryAnimator;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClickRight()
+    {
+        InventoryAnimator.SetBool("GoRight", true);
+    }
+    public void OnClickLeft()
+    {
+        InventoryAnimator.SetBool("GoLeft", true);
+    }
+}
