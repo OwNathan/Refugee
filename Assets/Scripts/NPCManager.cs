5d5bb8448b0fce0f436e7f61642cef1abeb34271
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-04-29 00:48:33 +0200

    Chiamatemi Bestemmia

---
+++ Assets/Scripts/NPCManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using AC;
+public class NPCManager : MonoBehaviour
+{
+
+    private GameObject Target;
+    private BoxCollider handsCollider;
+
+    public void Start()
+    {
+        Target = GameObject.FindObjectOfType<NPC>().gameObject;
+        if (Target != null)
+        {
+            handsCollider = Target.GetComponentInChildren<BoxCollider>();
+        }
+    }
+
+    public void OnHandsColliderEnable()
+    {
+        handsCollider.enabled = true;
+    }
+    public void OnHandsColliderDisable()
+    {
+        handsCollider.enabled = false;
+    }
+
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using AC;
+
+public class PlayerManager : MonoBehaviour
+{
+    private GameObject Target;
+    private BoxCollider handsCollider;
+
+    public void Start()
+    {
+        Target = GameObject.FindObjectOfType<Player>().gameObject;
+        if(Target != null)
+        {
+            handsCollider = Target.GetComponentInChildren<BoxCollider>();
+        }
+    }
+
+    public void OnHandsColliderEnable()
+    {
+        handsCollider.enabled = true;
+    }
+    public void OnHandsColliderDisable()
+    {
+        handsCollider.enabled = false;
+    }
+}
