648e6ed61ae93a48892d228721ce93987bb89d86
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-05-04 23:41:17 +0200

    Update both scenes

--- Assets/Scripts/NPCManager.cs
+++ Assets/Scripts/NPCManager.cs
@@ -1,28 +1,21 @@
 ﻿using UnityEngine;
-using System.Collections;
-using AC;
+
 public class NPCManager : MonoBehaviour
 {
-
-    private GameObject Target;
     private BoxCollider handsCollider;
 
     public void Start()
     {
-        Target = GameObject.FindObjectOfType<NPC>().gameObject;
-        if (Target != null)
-        {
-            handsCollider = Target.GetComponentInChildren<BoxCollider>();
-        }
+        handsCollider = this.GetComponentInChildren<BoxCollider>();
     }
 
     public void OnHandsColliderEnable()
     {
         handsCollider.enabled = true;
     }
+
     public void OnHandsColliderDisable()
     {
         handsCollider.enabled = false;
     }
-
 }
