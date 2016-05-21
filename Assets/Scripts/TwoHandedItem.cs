2a83df9b92e0841910504c8c9a78752991f49ae8
EnricoCempanari <enrico.cempanari@gmail.com>
2016-05-21 12:21:22 +0200

    Fixed Soldier animations, addes TwoHandBehavior, Fixed CH avatar mask, project cleaning

--- Assets/Scripts/TwoHandedItem.cs
+++ Assets/Scripts/TwoHandedItem.cs
@@ -7,10 +7,33 @@ public class TwoHandedItem : MonoBehaviour
     public Transform LeftHand;
     public GameObject Item;
 
-	void Update ()
+    [HideInInspector]
+    public bool IsTwoHanded;
+
+    private Animator animator;
+
+    void Awake()
     {
-        Item.transform.position = RightHand.transform.position;
-        Vector3 vDirection = LeftHand.position - RightHand.position;
-        Item.transform.rotation = Quaternion.LookRotation(vDirection);
+        animator = this.GetComponentInParent<Animator>();
+        IsTwoHanded = true;
+    }
+
+    void Update()
+    {
+        if (animator != null)
+        {
+            if (IsTwoHanded)
+            {
+                Item.transform.position = RightHand.transform.position;
+                Vector3 vDirection = LeftHand.position - RightHand.position;
+                Item.transform.rotation = Quaternion.LookRotation(vDirection);
+            }
+            else
+            {
+                Item.transform.position = RightHand.transform.position;
+            }
+        }
+
+
     }
 }
