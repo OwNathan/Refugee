8dc581cc61b1b075c37b25feb95c3ffa287be6fe
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-06-09 19:15:47 +0200

    Basic Setup CUSTOM LUA FUNCTION script

---
+++ Assets/LuaCustomFunctionsManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers;
+using PixelCrushers.DialogueSystem;
+
+public class LuaCustomFunctionsManager : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Lua.RegisterFunction("FindItem", this, typeof(LuaCustomFunctionsManager).GetMethod("FindItem"));
+    }
+
+    void OnDisable()
+    {
+        Lua.UnregisterFunction("FindItem");
+    }
+
+    public int FindItem(string name)
+    {
+        return 0;
+    }
+}
