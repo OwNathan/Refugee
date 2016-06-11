57f5b99bea40d6ddf002d3f1c4c6373de9333891
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-11 23:27:00 +0200

    Gui conversant positioning, all characters prefab updated. (WIP)
    Need to fix scene on scene management for gui positioning

--- Assets/GUIManager.cs
+++ Assets/GUIManager.cs
@@ -1,12 +1,96 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using PixelCrushers;
 using PixelCrushers.DialogueSystem;
+using AC;
+//TODO: field encapsulation
+//TODO: manage gui position for extreme camera shots
+//TODO: delocallizare GUIManager da dialogue manager e gettare i componenti della gui
 
 public class GUIManager : MonoBehaviour
 {
-    public void Update()
+    public List<string> ConversantNames;
+    public RectTransform CanvasRect;
+    public Image DialoguePanel;
+
+    private List<GameObject> Conversants;
+    private GameObject ActiveConversant;
+    private GameObject PreviousConversant;
+    private GameObject Rashid;
+    private Vector2 OriginalUIPosition;
+    private string CurrentConversantName;
+
+    void Awake()
     {
-        //DialogueManager.ConversationController.ConversantInfo.
+        Conversants = new List<GameObject>();
+        CurrentConversantName = "Not Set";
+        OriginalUIPosition = DialoguePanel.rectTransform.anchoredPosition;
+        ActiveConversant = new GameObject();
+        PreviousConversant = new GameObject();
+    }
+
+    void Start()
+    {
+        PopulateConversant();
+    }
+
+
+    public void LateUpdate()
+    {
+
+        if (DialogueManager.IsConversationActive)
+        {
+            //The character speaking
+            CurrentConversantName = DialogueManager.CurrentConversationState.subtitle.speakerInfo.Name;
+            //Find the speaking character in the scene
+            try
+            {
+                ActiveConversant = Conversants.Where(hC => hC.gameObject.tag == CurrentConversantName).FirstOrDefault();
+            }
+            catch (System.Exception)
+            {
+
+                throw new UnityException("Fix This pls!!!");
+            }
+            
+
+            
+            if(ActiveConversant != null && ActiveConversant != PreviousConversant)
+            {
+                //get custom offset for speaking character
+                GUIOffsetBehaviour HeadOffset = ActiveConversant.GetComponent<GUIOffsetBehaviour>();
+                Transform HeadPosition = HeadOffset.HeadPosition;
+                
+                Vector3 ScreenPos = KickStarter.mainCamera.attachedCamera._camera.WorldToScreenPoint(HeadPosition.position);
+                Vector2 RectPos = new Vector2();
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(CanvasRect, ScreenPos, null, out RectPos);
+
+                //set gui position
+                DialoguePanel.rectTransform.anchoredPosition = RectPos;
+                PreviousConversant = ActiveConversant;
+            }
+            else if (ActiveConversant == null)
+            {
+                DialoguePanel.rectTransform.anchoredPosition = OriginalUIPosition;
+                PreviousConversant = null;
+            }
+        }
+    }
+
+    public void PopulateConversant()
+    {
+        Conversants.Clear();
+        //Rashid needs to be acquired manually for his tag that cannot be changed
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Rashid = player.GetComponentInChildren<SkinnedMeshRenderer>().gameObject;
+        Conversants.Add(Rashid);
+        foreach (string name in ConversantNames)
+        {
+            Conversants.Add(GameObject.FindGameObjectWithTag(name));
+        }
     }
 }
---
+++ Assets/GUIOffsetBehaviour.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIOffsetBehaviour : MonoBehaviour
+{
+    public Transform HeadPosition;
+}
