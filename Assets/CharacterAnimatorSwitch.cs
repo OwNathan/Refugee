b2065ac5755381d5de2aff44c558e623a67f3792
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-12 22:39:16 +0200

    Created animator switch script. Updated disabled Heydar cutscene

---
+++ Assets/CharacterAnimatorSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimatorSwitch : MonoBehaviour
+{
+
+    public Animator DefaultAnimator;
+    public RuntimeAnimatorController AlternativeAnimator;
+
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void ChangeAnimator()
+    {
+        DefaultAnimator.runtimeAnimatorController = AlternativeAnimator;
+    }
+}
