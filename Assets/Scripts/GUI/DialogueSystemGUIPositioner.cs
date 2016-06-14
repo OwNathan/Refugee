ba5e4e19eaa4b63069d76901e54514fffb02d3d2
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-14 10:44:48 +0200

    Fixed GUIPositioner for x axis
    Y axis position still to do

--- Assets/Scripts/GUI/DialogueSystemGUIPositioner.cs
+++ Assets/Scripts/GUI/DialogueSystemGUIPositioner.cs
@@ -6,19 +6,41 @@ public class DialogueSystemGUIPositioner : MonoBehaviour
 {
 
     public RectTransform CanvasRect;
+    public RectTransform DialoguePanelTransform;
     public Image DialoguePanel;
+    public float PaddingX;
+    public float PaddingY;
 
     private Vector2 OriginalUIPosition;
+    private Vector2 Offset;
+    
 
     void Awake()
     {
         OriginalUIPosition = DialoguePanel.rectTransform.anchoredPosition;
+        Offset = new Vector2(PaddingX, PaddingY);
     }
+
+    //void Start()
+    //{
+    //    Debug.Log( CanvasRect.rect.xMax);
+
+    //}
+    
+
 
     public void SetConversantGUI(Vector2 ScreenPos)
     {
         Vector2 RectPos = new Vector2();
         RectTransformUtility.ScreenPointToLocalPointInRectangle(CanvasRect, ScreenPos, null, out RectPos);
+
+        Bounds DialoguePanelBounds = RectTransformUtility.CalculateRelativeRectTransformBounds(DialoguePanelTransform);
+        //Rect rect = CanvasRect.rect;
+        //DialoguePanelTransform.anchoredPosition.x + DialoguePanelBounds.extents.x > CanvasRect.rect.max.x
+        if (RectPos.x + DialoguePanelBounds.extents.x + Offset.x > CanvasRect.rect.xMax)
+            RectPos.x = CanvasRect.rect.xMax - DialoguePanelBounds.extents.x - Offset.x;
+        else if (RectPos.x - DialoguePanelBounds.extents.x - Offset.x < CanvasRect.rect.xMin)
+            RectPos.x = CanvasRect.rect.xMin + DialoguePanelBounds.extents.x + Offset.x;
         DialoguePanel.rectTransform.anchoredPosition = RectPos;
     }
 
