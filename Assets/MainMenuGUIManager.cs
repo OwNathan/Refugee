b6e1e76afcec932f374d47d92a15692a168953fb
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-18 21:24:51 +0200

    Added splash intro scene, main menu KS and greenlight buttons, project cleaning

---
+++ Assets/MainMenuGUIManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGUIManager : MonoBehaviour
+{
+
+    public void OpenKickStarterPage()
+    {
+        Application.OpenURL("https://www.kickstarter.com/");
+    }
+
+    public void OpenGreenlightPage()
+    {
+        Application.OpenURL("https://steamcommunity.com/greenlight/");
+    }
+}
