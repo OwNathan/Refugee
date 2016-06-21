68a03c0353be401a6ae12d5c89da434ca9a51aca
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-21 23:35:34 +0200

    Dialogue GUI updated, One response glitch fixed

---
+++ Assets/Scripts/GUI/ResponseButtonOneResponseMGR.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResponseButtonOneResponseMGR : MonoBehaviour
+{
+
+    public Button SecondResponseButton;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!SecondResponseButton.gameObject.activeSelf)
+            SecondResponseButton.gameObject.SetActive(true);
+    }
+}
