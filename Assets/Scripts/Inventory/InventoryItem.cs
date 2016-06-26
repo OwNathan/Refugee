0dc43702213f3a6a973a9bcd88bd236ad5562646
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-06-26 22:36:18 +0200

    Update Inventory

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -1,7 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-using System;
+﻿using System;
+using UnityEngine;
 
 //ITEM CONFIGURABILI TRAMITE LA FACTORY
 [Serializable]
@@ -9,17 +7,18 @@ public class InventoryItem
 {
     public string Name;
     public bool IsMultipleItem;
+    public int MaxCount;
     public Sprite Icon;
 
-    public InventoryItem(string Name, bool IsMultipleItem, Sprite Image)
+    public InventoryItem(string Name, bool IsMultipleItem, int MaxCount, Sprite Image)
     {
         this.Name = Name;
         this.IsMultipleItem = IsMultipleItem;
         this.Icon = Image;
+        this.MaxCount = MaxCount;
     }
 
-    public InventoryItem(InventoryItem item) : this(item.Name, item.IsMultipleItem, item.Icon)
+    public InventoryItem(InventoryItem item) : this(item.Name, item.IsMultipleItem, item.MaxCount, item.Icon)
     {
-        
     }
 }
