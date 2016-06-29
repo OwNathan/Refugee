065add73a5d4c2bee205a8e87e819b3c77772fab
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-29 16:00:52 +0200

    Bold for mouse over in menus

--- Assets/Scripts/DialogueSystem_SkipDialogue.cs
+++ Assets/Scripts/DialogueSystem_SkipDialogue.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using PixelCrushers.DialogueSystem;
-
+/// <summary>
+/// Called by UI full screen panel in order to skip dialogues just by clicking everywhere on the screen
+/// </summary>
 public class DialogueSystem_SkipDialogue : MonoBehaviour
 {
 
     public void CloseDialogue()
     {
+        
         ConversationController conversation = DialogueManager.ConversationController;
         if (conversation != null)
             conversation.Close();
