3d557d43f772597e4459185cfbe70ea5e6455218
EnricoCempanari <enrico.cempanari@gmail.com>
2016-06-29 17:54:20 +0200

    UI fullscreen skip button fixed (I hope)

---
+++ Assets/DialogueController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueController : MonoBehaviour
+{
+    /// <summary>
+    /// enables and disables full screen skip button to prevent unwanted dialogues skip during cutscenes
+    /// </summary>
+    public GameObject SkipButton;
+
+    void OnEnable()
+    {
+        SkipButton.SetActive(true);
+    }
+
+ void OnDisable()
+    {
+        SkipButton.SetActive(false);
+    }
+}
--- Assets/Scripts/DialogueSystem_SkipDialogue.cs
+++ Assets/Scripts/DialogueSystem_SkipDialogue.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 using PixelCrushers.DialogueSystem;
 /// <summary>
-/// Called by UI full screen panel in order to skip dialogues just by clicking everywhere on the screen
+/// Called in cutscenes to close dialogues when a scene need to be reloaded (eg. player death) in order to avoid dialogue desync
 /// </summary>
 public class DialogueSystem_SkipDialogue : MonoBehaviour
 {
 
     public void CloseDialogue()
     {
-        
+
         ConversationController conversation = DialogueManager.ConversationController;
         if (conversation != null)
             conversation.Close();
