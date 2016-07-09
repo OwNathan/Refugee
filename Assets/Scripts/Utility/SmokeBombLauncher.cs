fdb69c1b2250a2f6e6e9a87340183466e7e1aee8
EnricoCempanari <enrico.cempanari@gmail.com>
2016-07-09 19:06:49 +0200

    FF3 updated (need testing)

---
+++ Assets/Scripts/Utility/SmokeBombLauncher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmokeBombLauncher : MonoBehaviour
+{
+    public Transform LaunchTarget;
+    public float LaunchForce;
+    private Rigidbody smokeBombRigidbody;
+
+    void Awake()
+    {
+        smokeBombRigidbody = this.GetComponent<Rigidbody>();
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        Vector3 Trajectory = LaunchTarget.position - this.transform.position;
+        if (smokeBombRigidbody != null)
+        {
+            smokeBombRigidbody.AddForce(Trajectory * LaunchForce, ForceMode.Impulse);
+        }
+    }
+	
+    void FixedUpdate()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
