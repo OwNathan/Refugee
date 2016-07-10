9656cfe2fbc1214bde476132b1dfc1f0db3d6143
SaverioDiLazzaro <saverio.dilazzaro@gmail.com>
2016-07-10 15:00:48 +0200

    Uploaded Footstep SFX + Management + Updated PG prefabs

---
+++ Assets/SFXManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioSource FootstepSFX;
+    public float PitchVariation;
+    private float originalPitch;
+    
+    void Awake()
+    {
+        if (FootstepSFX != null)
+            originalPitch = FootstepSFX.pitch;
+    }
+	void PlayFootstepSFX()
+    {
+        if (FootstepSFX != null)
+        {
+            FootstepSFX.pitch = originalPitch + Random.Range(-PitchVariation, PitchVariation);
+            FootstepSFX.Play();
+        }
+    }
+}
