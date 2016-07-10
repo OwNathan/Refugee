e3513b347f6b619884f597d607bb7df8f25b64fd
EnricoCempanari <enrico.cempanari@gmail.com>
2016-07-10 18:21:42 +0200

    All scenes reset dialogue, fix dialogues blink on reposition. Lighting data for warehouse interior

--- Assets/Scripts/GUI/ResetDialogueOnSceneReloading.cs
+++ Assets/Scripts/GUI/ResetDialogueOnSceneReloading.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 using PixelCrushers.DialogueSystem;
 /// <summary>
+/// Called at start of every level to reset dialogues if active (like during quit to main menu)
 /// Called in cutscenes to close dialogues when a scene need to be reloaded (eg. player death) in order to avoid dialogue desync
+/// Needs to be placed in every scene
 /// </summary>
 public class ResetDialogueOnSceneReloading : MonoBehaviour
 {
+    void Start()
+    {
+        CloseDialogue();
+    }
 
     public void CloseDialogue()
     {
