7b6c1e926e1345a43dbd88804096e29724841b54
EnricoCempanari <enrico.cempanari@gmail.com>
2016-07-17 19:30:44 +0200

    FF3 Updated, DS updated with disclaimer image

---
+++ Assets/Scripts/Utility/EarlyBuildEnabler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+/// <summary>
+/// For Pre-Alpha build only enables and disable GUI Disclaimer Image
+/// </summary>
+public class EarlyBuildEnabler : MonoBehaviour
+{
+    public Image EarlyBuildImage;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.F10))
+            EarlyBuildImage.enabled = false;
+        else if (Input.GetKeyDown(KeyCode.F11))
+            EarlyBuildImage.enabled = true;
+    }
+}
